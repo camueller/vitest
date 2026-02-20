e1dabb25011aed00bf7c30b96bfeeb4003f86a52
Axel Müller <axel.a.mueller@deutschebahn.com>
2026-02-20 08:32:08 +0100

    Reproduce issue 9701

--- examples/basic/vite.config.ts
+++ examples/basic/vite.config.ts
@@ -1,12 +1,45 @@
-/// <reference types="vitest/config" />
+import { resolve } from 'node:path';
+import { defineConfig, mergeConfig } from 'vitest/config';
 
-// Configure Vitest (https://vitest.dev/config/)
+export default defineConfig(async () => {
+  const viteConfigFn = (await import('./vite.config.mjs')).default;
+  const viteConfig = viteConfigFn({ mode: 'test', command: 'serve' });
 
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  test: {
-    /* for example, use global to avoid globals imports (describe, test, expect): */
-    // globals: true,
-  },
-})
+  return mergeConfig(
+    viteConfig,
+    defineConfig({
+      test: {
+        pool: 'vmThreads',
+        reporters: ['default', ['html', { outputFile: resolve(process.cwd(), 'build/unit-test-report/index.html') }]],
+        name: 'unit-tests',
+        include: ['**/*.spec.ts'],
+        globals: true,
+        environment: `jsdom`,
+        setupFiles: [
+          './src/test/vitest.setup.ts',
+          './src/test/failTestOnConsoleError.ts',
+          './src/test/failOnMissingBuild.ts',
+        ],
+        coverage: {
+          provider: 'v8',
+          include: ['**/*.{ts,vue}'],
+          exclude: ['**/*.{d.ts,stories.ts}', 'test/**/*.ts'],
+          reportsDirectory: resolve(process.cwd(), 'build/coverage'),
+          reporter: [
+            'text',
+            // for coverage results aggregated for GitLab MR view
+            'cobertura',
+            'html',
+            // lcov for sonar-scanner
+            `lcov`,
+          ],
+          thresholds: {
+            functions: 89,
+            lines: 96,
+            branches: 82,
+          },
+        },
+      },
+    }),
+  );
+});
